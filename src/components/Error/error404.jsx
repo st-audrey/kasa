e1e7135827bba4ad07843ef07f7f1f0fc1a4a2e4
st-audrey <st_audrey@outlook.fr>
2023-05-03 17:23:37 +0200

    add router

---
+++ src/components/Error/error404.jsx
@@ -0,0 +1,17 @@
+function Error404() {
+
+    return (
+
+        <div>
+
+            <h1>Oups 🙈 Cette page n'existe pas</h1>
+
+        </div>
+
+    )
+
+}
+
+ 
+
+export default Error404
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+
+    return (
+        <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/about">A propos</Link>
+        </nav>
+    )
+
+}
+
+export default Header

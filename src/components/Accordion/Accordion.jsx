2f0a07f458712fb2842b43dfea7a8d48f7159c87
st-audrey <st_audrey@outlook.fr>
2023-05-05 17:59:23 +0200

    create accordion content

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -3,14 +3,19 @@ import carret from '../../assets/carret.png'
 
 function Accordion(props) {
   return (
-    <div className="accordion-container background-primary">
-      <div className="accordion-title-container">
-        <p className="accordion-title">{props.title}</p>
+    <>
+      <div className="accordion-container background-primary">
+        <div className="accordion-title-container">
+          <p className="accordion-title">{props.title}</p>
+        </div>
+        <div className="accordion-carret-container">
+          <img className="accordion-carret" src={carret} alt="..." />
+        </div>
       </div>
-      <div className="accordion-carret-container">
-        <img className="accordion-carret" src={carret} alt="..." />
+      <div className="accordion-content-container background-secondary">
+        <p className="accordion-content color-primary">{props.content}</p>
       </div>
-    </div>
+    </>
   )
 }
 

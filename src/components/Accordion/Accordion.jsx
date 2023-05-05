5ca0fbf3632c34b7961d1aeac4c72922d1f3eac9
st-audrey <st_audrey@outlook.fr>
2023-05-05 19:15:20 +0200

    create accordion opening

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,7 +1,15 @@
 import '../../styles/components/Accordion.css'
 import carret from '../../assets/carret.png'
+import { useState } from 'react'
 
 function Accordion(props) {
+  const [open, setOpen] = useState(false)
+
+  const toggle = (i) => {
+    setOpen(!open)
+    return i
+  }
+
   return (
     <>
       <div className="accordion-container background-primary">
@@ -9,10 +17,24 @@ function Accordion(props) {
           <p className="accordion-title">{props.title}</p>
         </div>
         <div className="accordion-carret-container">
-          <img className="accordion-carret" src={carret} alt="..." />
+          <img
+            className={
+              open ? 'accordion-carret accordion-carret-up' : 'accordion-carret'
+            }
+            src={carret}
+            alt="..."
+            onClick={toggle}
+          />
         </div>
       </div>
-      <div className="accordion-content-container background-secondary">
+
+      <div
+        className={
+          open
+            ? 'accordion-open background-secondary accordion-show'
+            : 'accordion-close background-secondary'
+        }
+      >
         <p className="accordion-content color-primary">{props.content}</p>
       </div>
     </>

0434e2c1c58674121afb1e19584ddf06a956afad
st-audrey <st_audrey@outlook.fr>
2023-05-06 14:16:05 +0200

    create card wrapper

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,10 @@
 import '../../styles/components/Card.css'
 
 function Card() {
-    // titreDeLaLocation
-    return (
-
-        <article className="card-container">
-            {/* <h3>{titreDeLaLocation}</h3> */}
-        </article>
-
-    )
-
+  // titreDeLaLocation
+  return (
+    <article className="card">{/* <h3>{titreDeLaLocation}</h3> */}</article>
+  )
 }
 
 export default Card
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,11 +1,32 @@
-import Banner from '../../components/Banner/Banner';
+import Banner from '../../components/Banner/Banner'
 import Card from '../../components/Card/Card'
-import '../../styles/Home.css';
+import '../../styles/Home.css'
+import Leasings from '../../data/logements.json'
 
 function Home() {
   return (
-    <><Banner /><Card /></>
+    <>
+      <Banner />
+      <div className="card-container background-secondary">
+        <div className="card-wrapper">
+          {Leasings.map((leasing) => (
+            <Card
+              key={leasing.id}
+              title={leasing.title}
+              cover={leasing.content}
+              pictures={leasing.pictures}
+              description={leasing.description}
+              host={leasing.host}
+              rating={leasing.rating}
+              location={leasing.location}
+              equipements={leasing.equipements}
+              tags={leasing.tags}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Home;
+export default Home

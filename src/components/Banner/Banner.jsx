72ad987916e8038cd523a28488d03df5ef76c1ea
st-audrey <st_audrey@outlook.fr>
2023-05-06 13:41:52 +0200

    fix banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,3 @@
-
 import '../../styles/components/Banner.css'
 
 import { useLocation } from 'react-router-dom'
@@ -6,24 +5,34 @@ import { useLocation } from 'react-router-dom'
 import banner_img from '../../assets/banner_img.webp'
 import about_img from '../../assets/about_img.webp'
 
-
 function Banner() {
+  const location = useLocation()
 
-    const location = useLocation()
+  let imageToDisplay = location.pathname === '/' ? banner_img : about_img
 
-    let imageToDisplay = location.pathname === "/" ? banner_img : about_img
+  return (
+    <div
+      className={
+        location.pathname === '/'
+          ? 'banner-container banner-common banner-home'
+          : 'banner-container banner-common banner-about'
+      }
+    >
+      <img
+        className={
+          location.pathname === '/'
+            ? 'banner-img banner-common'
+            : 'banner-img banner-about'
+        }
+        src={imageToDisplay}
+        alt="..."
+      />
 
-    return (
-        <div className='banner-container banner-common'>
-  
-            <img className='banner-img banner-common'src={imageToDisplay} alt="..."/>
-           
-            <p className= {location.pathname === "/" ? 'banner-txt' : 'd-none'}>Chez vous, partout et ailleurs</p>
-          
-        </div>
-   
-    )
-
+      <p className={location.pathname === '/' ? 'banner-txt' : 'd-none'}>
+        Chez vous, partout et ailleurs
+      </p>
+    </div>
+  )
 }
 
 export default Banner
